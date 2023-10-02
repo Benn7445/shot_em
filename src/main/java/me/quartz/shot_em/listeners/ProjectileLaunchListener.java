ee8f8e8fe7b16bb70c9b20e1bfebdba1ffb0e9ff
Ben Verbraecken <ben.verbraecken@gmail.com>
2023-10-03 00:40:24 +0200

    Sound Fixed

--- src/main/java/me/quartz/shot_em/listeners/ProjectileLaunchListener.java
+++ src/main/java/me/quartz/shot_em/listeners/ProjectileLaunchListener.java
@@ -3,6 +3,7 @@ import me.quartz.shot_em.ShotEm;
 import me.quartz.shot_em.game.Game;
 import me.quartz.shot_em.localPlayer.LocalPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -24,6 +25,7 @@ public class ProjectileLaunchListener implements Listener {
                         game.setShot(true);
                         Bukkit.getScheduler().scheduleSyncDelayedTask(ShotEm.getInstance(), () -> {
                             if (game.hasShot()) {
+                                player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1, 1);
                                 game.endGame();
                                 LocalPlayer localPlayer = ShotEm.getInstance().getLocalPlayerManager().getLocalPlayer(player);
                                 localPlayer.removePoints();
